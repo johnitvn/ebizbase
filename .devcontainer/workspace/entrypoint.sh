a5faae15051292c3baac93e631c83f0ce0a75639
John Martin <john.itvn@gmail.com>
2024-10-17 10:05:42 +0000

    chore: add devcontainer settings
    Added configuration files and scripts for setting up the development
    environment using DevContainer.
    
    - Added Docker Compose configuration in .devcontainer/compose.yml
    - Added DevContainer configuration in .devcontainer/devcontainer.json
    - Added Workspace service with Dockerfile and scripts
    - Added MailHog service with Dockerfile and scripts
    - Added MongoDB service with Dockerfile and scripts
    - Added MongoDB Sharded service with Dockerfile and scripts
    - Added Nginx configuration files
    - Added RabbitMQ configuration file

---
+++ .devcontainer/workspace/entrypoint.sh
@@ -0,0 +1,22 @@
+echo "======================================"
+echo "=        eBizBase DEVCONTAINER       ="
+echo "======================================"
+echo "Container started!"
+
+#!/bin/sh
+echo "Starting docker daemon"
+
+# Start Docker daemon
+dockerd&
+
+# Wait until Docker socket is available
+while [ ! -S /var/run/docker.sock ]; do
+  sleep 1
+done
+
+
+sleep 3
+echo '{"credsStore": "pass"}' > ~/.docker/config.json
+
+# Now execute any additional commands
+exec "$@"
